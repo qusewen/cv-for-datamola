b42bfc0feb719def480acd81d717280dc02e5d72
qusewen <quse.quse@mail.ru>
2023-01-15 15:04:54 +0300

    feat: update files

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 const SUMMARY = document.querySelector('.article__summary_subtitle-pre');
 const SUMMARY_BTN = document.querySelector('.article__info-btn');
-
-
+const BURGER_BTN = document.querySelector('.burger')
+const BURGER_CONTENT = document.querySelector('.burger__content')
 SUMMARY_BTN.addEventListener('click', ()=>{
     if(SUMMARY.style.display === 'block'){
         SUMMARY.style.display = 'none';
@@ -10,3 +10,20 @@ SUMMARY_BTN.addEventListener('click', ()=>{
     }
 })
 
+BURGER_BTN.addEventListener('click',()=>{
+    if(BURGER_CONTENT.style.display === 'block'){
+        BURGER_CONTENT.classList.add('animatedclose');
+        setTimeout(function(){
+            BURGER_CONTENT.style.display = 'none';
+            BURGER_CONTENT.classList.remove('animatedclose')
+        }, 400)
+        
+    }else{
+        BURGER_CONTENT.style.display = 'block';
+        BURGER_CONTENT.classList.add('animatedopen')
+        setTimeout(function(){
+            BURGER_CONTENT.classList.remove('animatedopen')
+        }, 500)
+    } 
+    
+} )
